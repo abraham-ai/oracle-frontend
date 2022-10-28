95f7a8c10365fb03f289c54beeb7fd7ccac39a33
Gene Kogan <kogan.gene@gmail.com>
2022-10-28 05:57:28 +0000

    maybe websocket is suboptimal

--- server.js
+++ server.js
@@ -10,44 +10,43 @@ const generator_url = config.generator_url;
 
 app.use(express.static('public'));
 
+// make app receive json
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', (socket) => {
-  console.log('Client connected');
+app.post('/run', async (req, res) => {
   
-  socket.on('disconnect', () => {
-    console.log('Client disconnected');
-  });
-  
-  socket.on('submit_oracle', async (data) => {
-    const faces = ["data/oracle.jpg", "data/oracle2.jpg", "data/oracle3.jpg"]
-    const faces_idx = Math.floor(Math.random() * faces.length);
-    const creation_config = {
-      "question": data.question,
-      "voice_embedding": "data/rivka_embedding.pkl",
-      "face": faces[faces_idx]
+  // get question from the post request body json
+  const question = req.body.question;
+  const faces = ["data/oracle.jpg", "data/oracle2.jpg", "data/oracle3.jpg"]
+  const faces_idx = Math.floor(Math.random() * faces.length);
+  const creation_config = {
+    "question": question,
+    "voice_embedding": "data/rivka_embedding.pkl",
+    "face": faces[faces_idx]
+  }
+  let results = await axios.post(`${generator_url}/run`, creation_config);
+  const task_id = results.data.token;
+  async function run_generator_update() {
+    results = await axios.post(`${generator_url}/fetch`, {token: task_id});
+    if (results.data.status.status == 'complete') {
+      let completion = results.data.output.completion;
+      let sha = results.data.output.sha;
+      res.status(200).send({
+        completion: completion,
+        sha: sha
+      });
+      return;
     }
-    let results = await axios.post(`${generator_url}/run`, creation_config);
-    const task_id = results.data.token;
-    async function run_generator_update() {
-      results = await axios.post(`${generator_url}/fetch`, {token: task_id});
-      if (results.data.status.status == 'complete') {
-        let completion = results.data.output.completion;
-        let sha = results.data.output.sha;
-        socket.emit('result', {
-          completion: completion,
-          sha: sha
-        });
-        return;
-      }
-      setTimeout(function(){
-        run_generator_update();
-      }, 5000);
-    }    
-    run_generator_update();    
-  });
+    setTimeout(function(){
+      run_generator_update();
+    }, 5000);
+  }    
+  run_generator_update();    
+
 });
 
 server.listen(3000, () => {
