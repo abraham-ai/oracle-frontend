b0faf51d608061d2c73a8960e9d9c9e7a5e4382c
Gene Kogan <kogan.gene@gmail.com>
2022-12-12 04:42:52 +0000

    update to new gateway

--- server.js
+++ server.js
@@ -5,8 +5,16 @@ const http = require('http');
 const axios = require('axios');
 const server = http.createServer(app);
 const io = require("socket.io")(server);
-const config = require('./config.json');
-const generator_url = config.generator_url;
+const dotenv = require('dotenv')
+
+dotenv.config()
+const EDEN_API_KEY = process.env.EDEN_API_KEY;
+const EDEN_API_SECRET = process.env.EDEN_API_SECRET;
+
+const GATEWAY_URL = "https://gateway-test.abraham.ai";
+//const MINIO_URL = "https://minio.aws.abraham.fun";
+//const MINIO_BUCKET = "creations-stg";
+
 
 app.use(express.static('public'));
 
@@ -18,45 +26,67 @@ app.get('/', (req, res) => {
 });
 
 app.post('/run', async (req, res) => {
-  
-  // get question from the post request body json
+
   const question = req.body.question;
   const faces = ["data/oracle.jpg", "data/oracle2.jpg", "data/oracle3.jpg"]
   const faces_idx = Math.floor(Math.random() * faces.length);
-  const creation_config = {
+  
+  const config = {
     "question": question,
     "voice_embedding": "data/rivka_embedding.pkl",
     "face": faces[faces_idx]
   }
-  console.log(creation_config, generator_url);
-  let results = await axios.post(`${generator_url}/run`, creation_config);
-  const task_id = results.data.token;
-  async function run_generator_update() {
-    results = await axios.post(`${generator_url}/fetch`, {token: task_id});
-    if (results.data.status.status == 'complete') {
-      let completion = results.data.output.completion;
-      let sha = results.data.output.sha;
-      console.log("Success, completion:", completion, "sha:", sha);
-      res.status(200).send({
-        completion: completion,
-        sha: sha
+
+  let authData = {
+    "apiKey": EDEN_API_KEY, 
+    "apiSecret": EDEN_API_SECRET
+  };
+  
+  let response = await axios.post(GATEWAY_URL+'/sign_in', authData)
+  let authToken = response.data.authToken;
+  
+  const request = {
+    "token": authToken,
+    "application": "oracle", 
+    "generator_name": "oracle", 
+    "config": config,
+    "metadata": {"question": question} 
+  }
+
+  response = await axios.post(GATEWAY_URL+'/request', request);
+  let prediction_id = response.data;
+  
+  console.log(`job submitted, task id ${prediction_id}`);
+
+  setInterval(async function() {
+    let response = await axios.post(GATEWAY_URL+'/fetch', {
+      "taskIds": [prediction_id]
+    });
+    
+    let {status, output} = response.data[0];
+    console.log(status, output)
+    
+    if (status == 'complete') {
+      //let outputUrl = `${MINIO_URL}/${MINIO_BUCKET}/${output}`;
+      console.log(`finished! result at ${output}`);
+      clearInterval(this);
+      return res.status(200).send({
+        completion: "",
+        sha: output
       });
-      return;
-    } else if (results.data.status.status == 'failed') {
-      console.log("Generator failed");
-      res.status(500).send({
+    }
+    else if (status == 'failed') {
+      console.log("failed");
+      clearInterval(this);
+      return res.status(500).send({
         error: "there was an error"
       });
-      return;
     }
-    setTimeout(async () => {
-      await run_generator_update();
-    }, 3000);
-  }    
-  run_generator_update();    
-
+  
+  }, 2000);
+  
 });
 
-server.listen(3000, () => {
-  console.log('Oracle listening on port 3000');
+server.listen(PORT, () => {
+  console.log(`Oracle listening on port ${PORT}`);
 });
