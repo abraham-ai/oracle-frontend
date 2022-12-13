7db298107d72e20251fc97c81f969066191cb86d
Gene Kogan <kogan.gene@gmail.com>
2022-12-13 06:29:59 +0000

    polling client side

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ dotenv.config()
 const EDEN_API_KEY = process.env.EDEN_API_KEY;
 const EDEN_API_SECRET = process.env.EDEN_API_SECRET;
 
-const GATEWAY_URL = "https://gateway-test.abraham.ai";
+const GATEWAY_URL = "https://app.dev.aws.abraham.fun"; //"https://gateway-test.abraham.ai";
 //const MINIO_URL = "https://minio.aws.abraham.fun";
 //const MINIO_BUCKET = "creations-stg";
 
@@ -60,34 +60,36 @@ app.post('/run', async (req, res) => {
   
   console.log(`job submitted, task id ${prediction_id}`);
 
-  setInterval(async function() {
-    let response = await axios.post(GATEWAY_URL+'/fetch', {
-      "taskIds": [prediction_id]
+  res.status(200).send({prediction_id: prediction_id});
+});
+
+
+app.post('/fetch', async (req, res) => {
+  const prediction_id = req.body.prediction_id;
+
+  let response = await axios.post(GATEWAY_URL+'/fetch', {
+    "taskIds": [prediction_id]
+  });
+  
+  let {status, output} = response.data[0];
+  console.log(status, output)
+  
+  if (status == 'failed') {
+    console.log("failed");
+    return res.status(500).send({
+      error: "there was an error"
     });
-    
-    let {status, output} = response.data[0];
-    console.log(status, output)
-    
-    if (status == 'complete') {
-      //let outputUrl = `${MINIO_URL}/${MINIO_BUCKET}/${output}`;
-      console.log(`finished! result at ${output}`);
-      res.status(200).send({
-        completion: "",
-        sha: output
-      });
-      console.log(`sent ${output} to client`)
-      clearInterval(this);
-    }
-    else if (status == 'failed') {
-      console.log("failed");
-      res.status(500).send({
-        error: "there was an error"
-      });
-      clearInterval(this);
-    }
-  
-  }, 2000);
-  
+  } 
+  else {
+    //let outputUrl = `${MINIO_URL}/${MINIO_BUCKET}/${output}`;
+    console.log(`finished! result at ${output}`);
+    return res.status(200).send({
+      completion: "",
+      sha: output,
+      status: status
+    });
+  }
+
 });
 
 server.listen(PORT, () => {
